228c7e19f482e16d267f88832612b71ca3c26f3f
Shazim Rahman <shazimrahman@hotmail.com>
2023-07-05 14:27:18 -0400

    add connection to DB

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('express-async-errors');
 
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect');
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -22,6 +23,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI);
         app.listen(port, () => {
             console.log(`Server is listening at http://localhost:${port}`);
         });
