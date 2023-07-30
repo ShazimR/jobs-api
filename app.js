0fbf425a349c7339e368a5678d63a3bd97813723
Shazim Rahman <shazimrahman@hotmail.com>
2023-07-29 20:09:54 -0400

    add routes & controllers for auth and login

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ const express = require('express');
 const app = express();
 const connectDB = require('./db/connect');
 
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Jobs API');
-});
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
